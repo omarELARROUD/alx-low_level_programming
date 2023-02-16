1836e8be3a40d669184d7566e7d1208ffe038572
OMARELARROUD <you@example.com>
2023-02-16 15:25:09 -0800

    ,l,

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/* dfgdfgd */
+
+/*dgrdfgd */
+int main(void)
+{
+/* dfdfdf */
+/*dfvgdfd */
+	printf("With proper grammar, but the outcome is a piece of art,\n");
+/* return */
+	return 0;
+}
