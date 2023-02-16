20f9b5cb8f6acfa348796660da69fc58e36aab48
OMARELARROUD <you@example.com>
2023-02-16 15:01:42 -0800

    lk,k

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 /**
- * this program print a quote 
- * the main function is the only function used
+ * this program print a quote
+ * main: the main function is the only function used
+ * return: exit with the value 0
  */
-int main(void) 
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
