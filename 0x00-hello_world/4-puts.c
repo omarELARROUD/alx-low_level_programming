6def425f18d49e29f728d487961e04c514a9eae7
OMARELARROUD <you@example.com>
2023-02-16 15:31:50 -0800

    ,l,

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
-/* this program print a quote */
-
-/* Description main */
+/**
+ * main - Entry point for the program
+ *
+ * Description: This function serves as the entry point for the program.
+ * It calls other functions to perform specific tasks, and returns an
+ * exit status to indicate the success or failure of the program.
+ *
+ * Return: 0 on success, non-zero on error
+ */
 int main(void)
 {
 /* main function to print sentence */
