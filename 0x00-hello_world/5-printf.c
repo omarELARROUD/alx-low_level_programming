f1283a7ed8633ec8ae78b21916434036a7fbf1de
OMARELARROUD <you@example.com>
2023-02-16 15:30:00 -0800

    ,l,

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,18 @@
 #include <stdio.h>
-/* dfgdfgd */
-
-/*dgrdfgd */
+/**
+ * main - Entry point for the program
+ *
+ * Description: This function serves as the entry point for the program.
+ * It calls other functions to perform specific tasks, and returns an
+ * exit status to indicate the success or failure of the program.
+ *
+ * Return: 0 on success, non-zero on error
+ */
 int main(void)
 {
 /* dfdfdf */
 /*dfvgdfd */
-	printf("With proper grammar, but the outcome is a piece of art,\n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 /* return */
-	return 0;
+	return (0);
 }
