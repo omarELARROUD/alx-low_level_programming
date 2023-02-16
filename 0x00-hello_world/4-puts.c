175f64d276c4baba304a0331a659454a6f748a7f
OMARELARROUD <you@example.com>
2023-02-16 14:56:01 -0800

    lk,k

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
-/* this program print a quote */
+/**
+ * this program print a quote 
+ * the main function is the only function used
+ */
 int main(void) 
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+	return (0);
 }
