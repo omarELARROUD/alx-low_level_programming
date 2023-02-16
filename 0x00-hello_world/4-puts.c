a63c54964313bde467e183f6c583a882a442765a
OMARELARROUD <you@example.com>
2023-02-16 13:50:13 -0800

    gn

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
-
+/* this program print a quote */
 int main() 
-    {
-    puts("Programming is like building a multilingual puzzle");
-    return 0;
-    }
+{
+    	puts("Programming is like building a multilingual puzzle");
+    	return 0;
+}
