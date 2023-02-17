5e415096809ffbe17ef63b8daa8d7124229a59aa
OMARELARROUD <you@example.com>
2023-02-17 15:12:46 -0800

    k,lk

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	n = 122;
+	while (n > 96)
+	{
+		putchar(n);
+		n--;
+	}
+	putchar('\n');
+	return (0);
+}
