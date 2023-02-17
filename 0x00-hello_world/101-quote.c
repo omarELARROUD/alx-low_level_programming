177ed270b5cd60d6011b66570965968db5ba51a3
OMARELARROUD <you@example.com>
2023-02-16 16:04:07 -0800

    jnk

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,18 @@
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 1 (Error)
+ */
+int main(void)
+{
+    const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+    /* Write the message to standard error using write() */
+    write(2, message, 59);
+
+    /* Return 1 to indicate an error occurred */
+    return (1);
+}
+
