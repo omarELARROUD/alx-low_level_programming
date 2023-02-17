020bd5a8b8f5d3c0c533be91bc0b62353d5b8ac6
OMARELARROUD <you@example.com>
2023-02-16 16:14:50 -0800

    jnk

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,10 +7,11 @@
  */
 int main(void)
 {
-	char *message ="and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	char *message = "and that piece of art is useful\"";
+	char *msg = "- Dora Korpar, 2015-10-19\n";
 
     /* Write the message to standard error using write() */
-	write(2, message, 59);
+	write(2, message+msg, 59);
 
     /* Return 1 to indicate an error occurred */
 	return (1);
