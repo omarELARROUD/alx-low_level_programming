2a4d66cb7a75238678540a48dce3ee1a92ec047d
OMARELARROUD <you@example.com>
2023-02-23 13:44:14 -0800

    hfjf

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <math.h>
+/**
+ * main - the fizz buzz program
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	long num = 612852475143;
+	long maxPrime = -1;
+	int i;
+
+	while(num%2 == 0)
+	{
+		maxPrime = 2;
+		num /= 2;
+	}
+	while(num%3 == 0)
+	{
+		maxPrime = 3;
+		num /= 3;
+	}
+	for(i=5; i<= sqrt(num); i+=6)
+	{
+		while(num%i == 0)
+		{
+			maxPrime = i;
+			num/=i;
+		}
+		while(num%(i+2) == 0)
+		{
+			maxPrime = i+2;
+			num/=(i+2);
+		}
+	}
+	if (num>4)
+		maxPrime = num;
+	printf("%ld\n", maxPrime);
+	return (0);
+}
