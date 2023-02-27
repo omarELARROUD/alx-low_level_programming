07cd8a24b812133c8e09812d72b42e19691d630a
OMARELARROUD <you@example.com>
2023-02-27 10:13:37 -0800

    k,l

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -3,8 +3,11 @@
 
 /**
  * *_strcpy a function that copies the string pointed to by src
+ *
  * @dest: a pointer to a buffer
+ *
  * @src: a pointer to a string
+ *
  * Return: Always 0.
  */
 char *_strcpy(char *dest, char *src)
