9ed91a71c841fd8567a7b8eb5475b3a47b2ea628
OMARELARROUD <you@example.com>
2023-03-20 16:36:24 -0700

    hh

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,28 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <string.h>
+/**
+ * init_dog - function that initialize a variable of type struct dog
+ *
+ * @d: struct dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ * Return: void
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d)
+	{
+		d->name = malloc(sizeof(char) * strlen(name));
+		if (!d->name)
+			exit(1);
+		d->name = name;
+		d->age = age;
+		d->owner = malloc(sizeof(char) * strlen(owner));
+		if (!d->owner)
+			exit(1);
+		d->owner = owner;
+	}
+}
