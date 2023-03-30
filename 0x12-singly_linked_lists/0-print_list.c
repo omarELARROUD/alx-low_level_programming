d11e063bcfe65cc33b07fc7a56cedc541d23bd94
OMARELARROUD <you@example.com>
2023-03-30 15:07:28 -0700

    jj

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,15 +10,18 @@
 size_t print_list(const list_t *h)
 {
 	int count = 0;
-
 	while (h)
 	{
 		if (h->str)
+		{
 			printf("[%d] %s\n", h->len, h->str);
+		}
 		else
+		{
 			printf("[0] (nil)\n");
-		h = h->next;
-		count++;
+			h = h->next;
+			count++;
+		}
 	}
 	return (count);
 }
