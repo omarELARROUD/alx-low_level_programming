07170785e771f152c50e8868a4b46f392e49521f
omarELARROUD <omar.elarroud13@gmail.com>
2023-07-07 00:55:37 +0100

    gre

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,17 +9,29 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
- 	unsigned int result = 0;
-    	if (b == NULL)
-        	return 0;
+	unsigned int ui;
+	int len, base_two;
 
-    	for (int i = 0; b[i] != '\0'; i++) 
+	if (!b)
+		return (0);
+
+	ui = 0;
+
+	for (len = 0; b[len] != '\0'; len++)
+		;
+
+	for (len--, base_two = 1; len >= 0; len--, base_two *= 2)
 	{
-        	if (b[i] != '0' && b[i] != '1')
-            		return 0;
+		if (b[len] != '0' && b[len] != '1')
+		{
+			return (0);
+		}
 
-        	result = (result * 2) + (b[i]-48);
-    	}
+		if (b[len] & 1)
+		{
+			ui += base_two;
+		}
+	}
 
-    	return result;
+	return (ui);
 }
